53277068477047f25fdae2abc7ba85901a1fe66c
unknown <montespierre@gmail.com>
2021-03-21 23:23:25 -0500

    ecommerce app

---
+++ app/src/main/java/com/weimont/ecommerceandroid/MainActivity.java
@@ -0,0 +1,16 @@
+package com.weimont.ecommerceandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //segunda actividad
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/weimont/ecommerceandroid/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.weimont.ecommerceandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //////////// Status bar hide start //////////////////////////////
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        }
+        //////////// Status bar hide end //////////////////////////////
+
+        ///////////// iniciar actividad con handler inicio //////////////////////////////
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
+        ///////////// iniciar actividad con handler fin //////////////////////////////
+
+
+    }
+}
