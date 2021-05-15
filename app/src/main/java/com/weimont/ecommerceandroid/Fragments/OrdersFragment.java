fe699eb67d4bcba9fd80040ab9eae926ad3a3d5f
unknown <montespierre@gmail.com>
2021-05-14 23:55:25 -0500

    fragments

--- app/src/main/java/com/weimont/ecommerceandroid/Fragments/OrdersFragment.java
+++ app/src/main/java/com/weimont/ecommerceandroid/Fragments/OrdersFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,7 +30,9 @@ public class OrdersFragment extends Fragment implements View.OnClickListener{
     ImageView navigationBar;
     NavigationView navigationView;
     private View view;
-    private TextView One, Two;
+    private RelativeLayout loginSignUp, bookmarks, fiorellaGold;
+    private TextView your_orders, online_ordering_help, address_book, favorite_orders, send_feedback, report_safety_emergency, rate_playstore;
+
 
 
 
@@ -38,6 +41,9 @@ public class OrdersFragment extends Fragment implements View.OnClickListener{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_orders, container, false);
+
+
+
         onSetNavigationDrawerEvents();
         return view;
     }
@@ -47,12 +53,32 @@ public class OrdersFragment extends Fragment implements View.OnClickListener{
         navigationView = (NavigationView) view.findViewById(R.id.navigationView);
 
         navigationBar = (ImageView) view.findViewById(R.id.navigationBar);
-        One = (TextView) view.findViewById(R.id.textView3);
-        Two = (TextView) view.findViewById(R.id.textView4);
+
+        loginSignUp = (RelativeLayout) view.findViewById(R.id.relativeLayout2);
+        bookmarks = (RelativeLayout) view.findViewById(R.id.relativeLayout3);
+        fiorellaGold = (RelativeLayout) view.findViewById(R.id.relativeLayout4);
+
+        your_orders = (TextView) view.findViewById(R.id.your_orders);
+        online_ordering_help = (TextView) view.findViewById(R.id.online_ordering_help);
+        address_book = (TextView) view.findViewById(R.id.address_book);
+        favorite_orders = (TextView) view.findViewById(R.id.favorite_orders);
+        send_feedback = (TextView) view.findViewById(R.id.send_feedback);
+        report_safety_emergency = (TextView) view.findViewById(R.id.report_safety_emergency);
+        rate_playstore = (TextView) view.findViewById(R.id.rate_playstore);
 
         navigationBar.setOnClickListener(this);
-        One.setOnClickListener(this);
-        Two.setOnClickListener(this);
+        loginSignUp.setOnClickListener(this);
+        bookmarks.setOnClickListener(this);
+
+        bookmarks.setOnClickListener(this);
+        fiorellaGold.setOnClickListener(this);
+        your_orders.setOnClickListener(this);
+        online_ordering_help.setOnClickListener(this);
+        address_book.setOnClickListener(this);
+        favorite_orders.setOnClickListener(this);
+        send_feedback.setOnClickListener(this);
+        report_safety_emergency.setOnClickListener(this);
+        rate_playstore.setOnClickListener(this);
 
     }
 
@@ -62,11 +88,35 @@ public class OrdersFragment extends Fragment implements View.OnClickListener{
             case R.id.navigationBar:
                 drawerLayout.openDrawer(navigationView, true);
                 break;
-            case R.id.textView3:
-                Toast.makeText(getContext(),"One",Toast.LENGTH_SHORT).show();
+            case R.id.relativeLayout2:
+                Toast.makeText(getContext(),"loginSignUp",Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.textView4:
-                Toast.makeText(getContext(),"Two",Toast.LENGTH_SHORT).show();
+            case R.id.relativeLayout3:
+                Toast.makeText(getContext(),"bookmarks",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.relativeLayout4:
+                Toast.makeText(getContext(),"fiorellaGold",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.your_orders:
+                Toast.makeText(getContext(),"your_orders",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.online_ordering_help:
+                Toast.makeText(getContext(),"online_ordering_help",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.address_book:
+                Toast.makeText(getContext(),"address_book",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.favorite_orders:
+                Toast.makeText(getContext(),"favorite_orders",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.send_feedback:
+                Toast.makeText(getContext(),"send_feedback",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.report_safety_emergency:
+                Toast.makeText(getContext(),"report_safety_emergency",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rate_playstore:
+                Toast.makeText(getContext(),"rate_playstore",Toast.LENGTH_SHORT).show();
                 break;
 
         }
