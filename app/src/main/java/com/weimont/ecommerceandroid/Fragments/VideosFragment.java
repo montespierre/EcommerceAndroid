0b554917cb5854dc2c0fecc71e2595e3f47eaf48
unknown <montespierre@gmail.com>
2021-05-15 20:11:35 -0500

    fragments

--- app/src/main/java/com/weimont/ecommerceandroid/Fragments/VideosFragment.java
+++ app/src/main/java/com/weimont/ecommerceandroid/Fragments/VideosFragment.java
@@ -2,65 +2,121 @@ package com.weimont.ecommerceandroid.Fragments;
 
 import android.os.Bundle;
 
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.weimont.ecommerceandroid.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VideosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class VideosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class VideosFragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public VideosFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VideosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VideosFragment newInstance(String param1, String param2) {
-        VideosFragment fragment = new VideosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    DrawerLayout drawerLayout;
+    ImageView navigationBar;
+    NavigationView navigationView;
+    private View view;
+    private RelativeLayout loginSignUp, bookmarks, fiorellaGold;
+    private TextView your_orders, online_ordering_help, address_book, favorite_orders, send_feedback, report_safety_emergency, rate_playstore;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videos, container, false);
+        view = inflater.inflate(R.layout.fragment_videos, container, false);
+        onSetNavigationDrawerEvents();
+        return view;
     }
+
+    private void onSetNavigationDrawerEvents() {
+        drawerLayout = (DrawerLayout) view.findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) view.findViewById(R.id.navigationView);
+
+        navigationBar = (ImageView) view.findViewById(R.id.navigationBar);
+
+        loginSignUp = (RelativeLayout) view.findViewById(R.id.relativeLayout2);
+        bookmarks = (RelativeLayout) view.findViewById(R.id.relativeLayout3);
+        fiorellaGold = (RelativeLayout) view.findViewById(R.id.relativeLayout4);
+
+        your_orders = (TextView) view.findViewById(R.id.your_orders);
+        online_ordering_help = (TextView) view.findViewById(R.id.online_ordering_help);
+        address_book = (TextView) view.findViewById(R.id.address_book);
+        favorite_orders = (TextView) view.findViewById(R.id.favorite_orders);
+        send_feedback = (TextView) view.findViewById(R.id.send_feedback);
+        report_safety_emergency = (TextView) view.findViewById(R.id.report_safety_emergency);
+        rate_playstore = (TextView) view.findViewById(R.id.rate_playstore);
+
+        navigationBar.setOnClickListener(this);
+        loginSignUp.setOnClickListener(this);
+        bookmarks.setOnClickListener(this);
+
+        bookmarks.setOnClickListener(this);
+        fiorellaGold.setOnClickListener(this);
+        your_orders.setOnClickListener(this);
+        online_ordering_help.setOnClickListener(this);
+        address_book.setOnClickListener(this);
+        favorite_orders.setOnClickListener(this);
+        send_feedback.setOnClickListener(this);
+        report_safety_emergency.setOnClickListener(this);
+        rate_playstore.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.navigationBar:
+                drawerLayout.openDrawer(navigationView, true);
+                break;
+            case R.id.relativeLayout2:
+                Toast.makeText(getContext(),"loginSignUp",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.relativeLayout3:
+                Toast.makeText(getContext(),"bookmarks",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.relativeLayout4:
+                Toast.makeText(getContext(),"fiorellaGold",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.your_orders:
+                Toast.makeText(getContext(),"your_orders",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.online_ordering_help:
+                Toast.makeText(getContext(),"online_ordering_help",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.address_book:
+                Toast.makeText(getContext(),"address_book",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.favorite_orders:
+                Toast.makeText(getContext(),"favorite_orders",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.send_feedback:
+                Toast.makeText(getContext(),"send_feedback",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.report_safety_emergency:
+                Toast.makeText(getContext(),"report_safety_emergency",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rate_playstore:
+                Toast.makeText(getContext(),"rate_playstore",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+
+
+
 }
